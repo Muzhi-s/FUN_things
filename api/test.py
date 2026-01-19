6c66d1c447b2326f38d4eda9d122de693bf7133c
Muzhi-s <2624758301@qq.com>
2026-01-19 22:07:39 +0800

    Fix vercel python runtime and handler

--- api/test.py
+++ api/test.py
@@ -1,7 +1,2 @@
-from http.server import BaseHTTPRequestHandler
-
-class Handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(b"Hello")
+def handler(request):
+    return "Hello from Vercel Python API"
